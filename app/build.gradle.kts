e10b5630232a799638532e6822e8c12fbdd911a8
Oleg Skakunov <dosmds@gmail.com>
2026-05-31 15:53:06 +0300

    Prepare release 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.dosmds.cipheralphabet"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
